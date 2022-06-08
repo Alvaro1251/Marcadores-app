537bb3f97a9c1c4c24635a8968eb58e68f337faa
Alvaro Signorio <90202428+Alvaro1251@users.noreply.github.com>
2022-06-08 02:44:04 -0300

    production deploy

--- src/keys.js
+++ src/keys.js
@@ -1,12 +1,21 @@
+
 require('dotenv').config();
 
 module.exports = {
 
-    database:{
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: '',
-        database: process.env.DB_NAME
+    "database":{
+        "host": "localhost",
+        "user": "root",
+        "password": '',
+        "database": "datebase_links"
+    },
+    "production":{
+        "host": process.env.DB_HOST,
+        "user": process.env.DB_USER,
+        "password": '',
+        "database": process.env.DB_NAME,
+        "dialect": "mysql",
+        "operatorsAliases": 0
     }
 
 };
