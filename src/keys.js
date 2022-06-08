e337b13105dcd6b7ba9466b3148745702313fba3
Alvaro Signorio <90202428+Alvaro1251@users.noreply.github.com>
2022-06-07 22:44:49 -0300

    favicon err fix

--- src/keys.js
+++ src/keys.js
@@ -5,7 +5,7 @@ module.exports = {
     database:{
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
-        password: process.env.DB_PASS,
+        password: '',
         database: process.env.DB_NAME
     }
 
